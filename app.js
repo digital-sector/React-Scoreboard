b819bda1c655b0717842bacb4990828b3cacc8c8
digital-sector <specificvector@protonmail.com>
2022-06-07 19:12:52 +1000

    Added scoreboard boilerplate.

--- app.js
+++ app.js
@@ -1,16 +1,87 @@
-const desc = "I just learned to render react elements to a page.";
-const myTitleId = 'main-title';
-const name = 'Guil';
+const players = [  {
+    name: "Guil",
+    score: 50,
+    id: 1,
+  },
+  {
+    name: "Treasure",
+    score: 85,
+    id: 2,
+  },
+  {
+    name: "Ashley",
+    score: 95,
+    id: 3,
+  },
+  {
+    name: "James",
+    score: 80,
+    id: 4,
+  }
+];
+
+function Header(props) {
+    console.log(props)
+    return (
+        <header>
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayers }</span>
+        </header>
+    );
+};
+
+class Counter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            score: 0
+        }
+    }
 
 
-const header = (
-    <header>
-        <h1 id={ myTitleId }>{ name }'s First React App</h1>
-        <p className="main-desc">{ desc }</p>
-    </header>
-);
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score">{ this.props.score }</span>
+                <button className="counter-action decrement"> + </button>
+            </div>
+        );   
+    }
+}
+
+function Player(props) {
+    return (
+        <div className="player">
+            <span className="player-name">{ props.name }</span>
+            <Counter 
+                score={ props.score }
+            />
+        </div>
+    );
+};
+
+function App(props) {    
+    return (
+        <div className="scoreboard">
+            <Header 
+                title="Scoreboard" 
+                totalPlayers={ props.initalPlayers.length } 
+            />
+            {props.initalPlayers.map( player => 
+            <Player 
+                name={ player.name } 
+                score={ player.score }
+                key={ player.id.toString() }
+            /> 
+            )}
+        </div>
+    );
+};
 
 ReactDOM.render(
-    header, 
+    <App 
+        initalPlayers={players}
+    />, 
     document.getElementById('root')
-    );
+);
